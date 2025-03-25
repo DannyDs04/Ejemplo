fd037280c620d7a66ca8d6746a7567f852e70834
Danny Santiago Buesaquillo Pabon <danny.buesaquillo@udea.edu.co>
2025-03-25 17:48:43 -0500

    Add files via upload

---
+++ Clase_ejercicios.py
@@ -0,0 +1,23 @@
+
+#Ejercicio numero uno de la clase. Mulplicando usando sumas!
+
+numero = int(input("Ingrese un numero: "))
+multiplicador = int(input("Ingrese un numero: "))
+   
+i = 0
+resultado = 0
+
+while i < numero:
+  resultado += multiplicador
+  print(resultado)
+  i += 1
+  
+print("El resultado es: ",resultado)
+
+#Ejerjcicio 2. 
+
+nn = int(input("Ingresar numero de notas: "))
+notas = float(input("Ingresar el valor de la nota: "))
+
+n1 = n1 + 
+while 
